da19a71627be4ba8cfb7aebb6a8a9ad078d9a8d4
Ravy <smart.fz@mail.ru>
2019-10-18 00:00:23 +0300

    Финал 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,25 +6,22 @@ module.exports = {
     MVIDEO_LINK: 'https://www.mvideo.ru/products/igrovye-naushniki-logitech-g432-981-000770-50126861',
   },
   head: {
-    title: 'Logitech G432 Gaming Headsets',
-    
+    title: 'Игровая гарнитура Logitech G432 с поддержкой технологии объемного звучания 7.1',
     meta: [
-      {
-        charset: 'utf-8'
-      },
-      {
-        name: 'author',
-        content: 'Ravy & Seenix'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'About Logitech'
-      }
+      { charset: 'utf-8' },
+      { name: 'author', content: 'Ravy & Seenix' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Игровая гарнитура Logitech G432 с поддержкой технологии объемного звучания 7.1' },
+      { name: 'twitter:description', content: 'В универсальной игровой гарнитуре G432 с облегченной конструкцией используются диффузоры диаметром 50 мм, технология объемного звучания DTS, 6-миллиметровый микрофон и многое другое.' },
+      { name: 'og:title', content: 'Игровая гарнитура Logitech G432 с поддержкой технологии объемного звучания 7.1' },
+      { name: 'og:description', content: 'В универсальной игровой гарнитуре G432 с облегченной конструкцией используются диффузоры диаметром 50 мм, технология объемного звучания DTS, 6-миллиметровый микрофон и многое другое.' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'http://headsets.logitech.promo' },
+      { name: 'og:locale', content: 'ru_RU' },
+      { name: 'twitter:image', content: 'https://www.logitechg.com/content/dam/gaming/en/products/g432/g432-hero.png.imgw.1318.1318.jpeg' },
+      { name: 'og:image', content: 'https://www.logitechg.com/content/dam/gaming/en/products/g432/g432-hero.png.imgw.1318.1318.jpeg' },
+      { hid: 'description', name: 'description', content: 'В универсальной игровой гарнитуре G432 с облегченной конструкцией используются диффузоры диаметром 50 мм, технология объемного звучания DTS, 6-миллиметровый микрофон и многое другое.' }
     ],
     link: [
       {
